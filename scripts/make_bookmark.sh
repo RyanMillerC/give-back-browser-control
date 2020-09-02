81a55fce98aad300c660e2a90cc93352cb4accca
Ryan Miller <ryan@devopsmachine.com>
2020-09-02 02:20:12 -0400

    Add javascript: to beginning of bookmark

--- scripts/make_bookmark.sh
+++ scripts/make_bookmark.sh
@@ -3,7 +3,9 @@
 # Minify giveBackControl.js to create giveBackControlBookmark.js
 #
 
+printf 'javascript:' > giveBackControlBookmark.js
+
 # Remove leading spaces, empty lines, then replace line endings with spaces
 sed -e '/^[[:space:]]*$/d' \
     -e 's/^[ \t]*//' \
-    src/giveBackControl.js | tr '\n' ' ' > giveBackControlBookmark.js
+    src/giveBackControl.js | tr '\n' ' ' >> giveBackControlBookmark.js
