7e409b6516a901f97c130be1f4e96abe7c92af70
Ryan Miller <ryan@devopsmachine.com>
2020-09-02 01:36:01 -0400

    Change path and output path in make_bookmark.sh

--- scripts/make_bookmark.sh
+++ scripts/make_bookmark.sh
@@ -6,4 +6,4 @@
 # Remove leading spaces, empty lines, then replace line endings with spaces
 sed -e '/^[[:space:]]*$/d' \
     -e 's/^[ \t]*//' \
-    giveBackControl.js | tr '\n' ' '
+    src/giveBackControl.js | tr '\n' ' ' > giveBackControlBookmark.js
